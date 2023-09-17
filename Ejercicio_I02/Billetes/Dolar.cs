0258c5b63de1e7a04b7e4ad6aeabfacbaeff5ead
Agus <107086899+Agus0503@users.noreply.github.com>
2023-09-17 16:45:26 -0300

    Sobrecarga

---
+++ Ejercicio_I02/Billetes/Dolar.cs
@@ -0,0 +1,34 @@
+﻿namespace Billetes
+{
+    public class Dolar
+    {
+        private double cantidad;
+        private static double cotzRespectoDolar;
+
+        public Dolar() { }
+
+        static Dolar()
+        {
+            cotzRespectoDolar = 1;
+        }
+
+        public Dolar(double cantidad)
+        {
+            this.cantidad = cantidad;
+        }
+
+        public double GetCantidad() => cantidad;
+        public static double GetCotizacion() => cotzRespectoDolar;
+                
+        public static explicit operator Euro(Dolar d)
+        {
+            return new Euro(d.GetCantidad() / Euro.GetCotizacion());
+        }
+
+        public static explicit operator Peso(Dolar d)
+        {
+            return new Peso(d.GetCantidad() * Peso.GetCotizacion());
+        }
+
+    }
+}
